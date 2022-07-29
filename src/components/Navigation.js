475fc18c2962e15a4ab16755c9ae399bcef745a9
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-07-29 01:56:10 -0500

    C2-30:30

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component{
+   render(){
+    return(
+       <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <a href="#" className="btn btn-primary stretched-link">
+                    Task
+                </a>
+            </nav>
+       </div> 
+    );
+   };
+};
+
+export default Navigation;
