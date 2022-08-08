78346cbe32ae0de37c6b3b1b821582b84ce2d476
beartree-ars <arthur.rs0313@gmail.com>
2022-08-08 09:59:30 -0500

    Example of states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
+import {todos} from './todos.json';
 
-function App() {
-    return (
+{/*Estados*/}
+console.log(todos);
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      title: "Aplicacion de tareas",
+      ntarea: 10
+    };
+  }  
+
+
+    render(){
+      return (
       <div className="App">
-        <Navigation titulo="Mi primera navegacion"/>
-                    
+          {/*Componente en src/components/Navigation*/}
+          <Navigation titulo="Task">   
+          </Navigation>
+          {/*Estado*/}
+          <nav className="navbar navbar-dark bg-dark">  
+            <p href="#" className="text-light stretched-link">
+              {this.state.title} - {this.state.ntarea}
+            </p>
+          </nav>
         <img src={logo} className="App-logo" alt="logo" />
-          
       </div>
     );
+    }
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,6 +7,7 @@ class Navigation extends Component{
             <nav className="navbar navbar-dark bg-dark">
                 <a href="#" className="btn btn-primary stretched-link">
                     {this.props.titulo} 
+                    
                 </a>
             </nav>
        </div> 
